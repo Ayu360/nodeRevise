ff35cced6df84309091e2f88c9b1d360856a9642
Ayush Singh <ayushandsingh987654321@gmail.com>
2024-03-12 22:39:35 +0530

    comments added!

--- 12-http-module.js
+++ 12-http-module.js
@@ -5,7 +5,19 @@ const server = http.createServer((req, res) => {
   if (req.url === "/") {
     res.end("Welcome to our home page");
   } else if (req.url === "/about") {
+    for (let i = 0; i < 1000; i++) {
+      for (let j = 0; j < 1000; j++) {
+        console.log(`${i} ${j}`);
+      }
+    }
     res.end("Here is our short history");
+  } else if (req.url === "/product") {
+    // for (let i = 0; i < 1000; i++) {
+    //   for (let j = 0; j < 1000; j++) {
+    //     console.log(`${i} ${j}`);
+    //   }
+    // }
+    res.end("Here are our products");
   } else {
     res.end(`
         <h1>Oops!</h1>
@@ -16,3 +28,11 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(5000);
+
+/* What we see here is even though the blocking code (double for loop) is
+present only in about section but if ever this resource is requested, all
+other requests also have to suffer (load about page and then load other 
+pages, you will see lots of loading). So we need to handle it asyncronously!
+Since then all these blocking codes will be "ofloaded" and will be executed 
+only when our res is ready!
+ */
